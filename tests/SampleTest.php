34f137eaf7b4dc913b4c46cfd6bd82eed41744f2
ndaDayo <hayato711@gmail.com>
2020-03-07 16:12:28 +0900

    文字Rを渡した時、文字列Yesを返す[Fail]

---
+++ tests/SampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function 文字列Rを渡した時、文字列YESを返す()
+    {
+        $sample = new Sample();
+        $result = $sample->solve('R');
+        $this->assertSame('Yes',$result);
+    }
+}
