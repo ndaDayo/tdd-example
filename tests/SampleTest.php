9fa8998020f597623e24bd723f70cc65d6eea399
ndaDayo <hayato711@gmail.com>
2020-03-07 16:45:00 +0900

    文字列UULを渡した時、文字列Noを返すテスト追加[FAIL]

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -31,4 +31,11 @@ class SampleTest extends TestCase
         $this->assertSame('No',$this->sample->solve('L'));
     }
 
+    /**
+     * @test
+     */
+    public function 文字列UULを渡した時、文字列Noを返す()
+    {
+        $this->assertSame('No',$this->sample->solve('UUL'));
+    }
 }
