c5883b2a55015478599d7e35976b432c34f4768c
ndaDayo <hayato711@gmail.com>
2020-03-07 17:43:00 +0900

    偶数と奇数の判定処理分けた

--- App/Sample.php
+++ App/Sample.php
@@ -10,10 +10,20 @@ class Sample
 
         for ($i = 0; $i < $len; $i++) {
             $currentChar = $input[$i];
-            if ($i % 2 === 0 && $currentChar === 'L' || $i % 2 === 1 && $currentChar === 'R') {
+            if ($this->isOddCharL($i, $currentChar) || $this->isEvenCharL($i, $currentChar)) {
                 return 'No';
             }
         }
         return 'Yes';
+    }
+
+    private function isOddCharL($index, $currentChar): bool
+    {
+        return $index % 2 === 0 && $currentChar === 'L';
+    }
+
+    private function isEvenCharL($index, $currentChar): bool
+    {
+        return $index % 2 === 1 && $currentChar === 'R';
     }
 }
