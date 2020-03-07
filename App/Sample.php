6ae1c27d6e2fc415a1b3496d1f230c08718c23f4
ndaDayo <hayato711@gmail.com>
2020-03-07 16:57:43 +0900

    文字列UULを渡した時、文字列Noを返す実装追加[PASS]

--- App/Sample.php
+++ App/Sample.php
@@ -6,8 +6,12 @@ class Sample
 {
     public function solve(string $input): string
     {
-        if ($input === 'L') {
-            return 'No';
+        $len = strlen($input);
+
+        for ($i = 0; $i < $len; $i += 2) {
+            if ($input[$i] === 'L') {
+                return 'No';
+            }
         }
         return 'Yes';
     }
